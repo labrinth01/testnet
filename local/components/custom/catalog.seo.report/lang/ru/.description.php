f7d0475ac17b7ee320202f16c0af8c1a49dd7e7d
labrinth01 <dimavmajnkraft@gmail.com>
2026-09-18 19:42:25 +0500

    Delete local/components/custom/catalog.seo.report/lang/ru directory

--- local/components/custom/catalog.seo.report/lang/ru/.description.php
+++
@@ -1,5 +0,0 @@
-<?php
-$MESS["CUSTOM_SEO_REPORT_NAME"] = "SEO-отчёт по товарам";
-$MESS["CUSTOM_SEO_REPORT_DESC"] = "Список товаров каталога с проверкой заполненности карточки и оценкой SEO-готовности 0–100%";
-$MESS["CUSTOM_SEO_REPORT_PATH"] = "Компоненты проекта";
-$MESS["CUSTOM_SEO_REPORT_PATH_CHILD"] = "Каталог";
--- local/components/custom/catalog.seo.report/lang/ru/.parameters.php
+++
@@ -1,6 +0,0 @@
-<?php
-$MESS["CUSTOM_SEO_GROUP_FILTER"] = "Фильтр";
-$MESS["CUSTOM_SEO_IBLOCK_TYPE"] = "Тип инфоблока";
-$MESS["CUSTOM_SEO_IBLOCK_ID"] = "Инфоблок";
-$MESS["CUSTOM_SEO_DETAIL_URL"] = "URL страницы товара";
-$MESS["CUSTOM_SEO_CACHE_GROUPS"] = "Учитывать права доступа";
